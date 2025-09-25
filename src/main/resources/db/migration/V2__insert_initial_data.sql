454c5a4a93bd3107532c7844e9290fdd58101ce6
AndreiPopescu <andrei1.popescu1@gmail.com>
2025-09-25 21:51:03 +0300

    add 100k orders

--- src/main/resources/db/migration/V2__insert_initial_data.sql
+++ src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -6,5 +6,3 @@ VALUES ('Labubu', 10.00),
 INSERT INTO customer (name, password)
 VALUES ('AndreiPopescu', '1234');
 
-INSERT INTO orders (order_date, total_amount, customer_id)
-VALUES ('2025-09-24', 89.97, 1);
---
+++ src/main/resources/db/migration/V3__insert_orders.sql
@@ -0,0 +1,11 @@
+DO $$
+    DECLARE
+        amt numeric;
+    BEGIN
+        FOR i IN 1..100000 LOOP
+                amt := (random()*200 + 10)::numeric;  -- cast only, no rounding
+                INSERT INTO orders (order_date, total_amount, customer_id)
+                VALUES ('2025-09-24'::date + (i % 30), amt, 1);
+            END LOOP;
+    END
+$$;

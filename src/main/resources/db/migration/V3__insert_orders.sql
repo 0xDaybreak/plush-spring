d9beec9d6b3ba4d16f72add33847bcca7efd3df3
AndreiPopescu <andrei1.popescu1@gmail.com>
2025-09-25 22:15:04 +0300

    add 500k orders

--- src/main/resources/db/migration/V3__insert_orders.sql
+++ src/main/resources/db/migration/V3__insert_orders.sql
@@ -2,7 +2,7 @@ DO $$
     DECLARE
         amt numeric;
     BEGIN
-        FOR i IN 1..100000 LOOP
+        FOR i IN 1..500000 LOOP
                 amt := (random()*200 + 10)::numeric;  -- cast only, no rounding
                 INSERT INTO orders (order_date, total_amount, customer_id)
                 VALUES ('2025-09-24'::date + (i % 30), amt, 1);
